c3332299ffee6d8f09a28d92a655ef55f62c8025
phanindravarma412 <phanindravarma412@gmail.com>
2020-12-30 00:21:54 +0530

    added dynamic slider and textfield

--- lib/main.dart
+++ lib/main.dart
@@ -22,73 +22,75 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-
-
   List<DynamicWidget> widgets = List<DynamicWidget>();
   // List<bool> checkValues = List<bool>();
-
+  
+  List<TextEditingController> listNamecontrollers = [];
   addWidget() {
     setState(() {
+      TextEditingController nameController = TextEditingController();
+      listNamecontrollers.add(nameController);
       widgets.insert(
           widgets.length,
           DynamicWidget(
-            checkValue: false,
-          ));
+              firstCheckValue: false,
+              secondCheckValue: false,
+              thirdCheckValue: false,
+              initialValue: 1.0,
+              controller: listNamecontrollers[listNamecontrollers.length - 1]));
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: <Widget>[
-        ListView.builder(
-          shrinkWrap: true,
-          itemCount: widgets.length,
-          itemBuilder: (context, index) {
-            return widgets[index];
-          },
-        ),
-        RaisedButton(
-          child: Text("click"),
-          onPressed: () {
-            addWidget();
-          },
-        ),
-        FlatButton(
-          child: Text('GetData'),
-          color: Colors.green,
-          onPressed: () {
-            widgets.length == 0 ? print('Empty') : print(widgets[0]);
-            // print(widgets[0].checkValue);
+    return SingleChildScrollView(
+      child: Column(
+        children: <Widget>[
+          ListView.builder(
+            shrinkWrap: true,
+            itemCount: widgets.length,
+            itemBuilder: (context, index) {
+              return widgets[index];
+            },
+          ),
+          RaisedButton(
+            child: Text("click"),
+            onPressed: () {
+              addWidget();
+            },
+          ),
+          FlatButton(
+            child: Text('GetData'),
+            color: Colors.green,
+            onPressed: () {
+              widgets.length == 0 ? print('Empty') : print(widgets[0]);
 
-            for(int i =0; i<widgets.length; i++){
-              print('$i value is ${widgets[i].checkValue}');
-            }
-
-
-            // // print(widgets[0]);
-            // // print(DynamicWidget().getString);
-            // print(widgets[0]);
-            
-            // // widgets.map((widget) {
-            // //   print(widget);
-            // // });
-          },
-        )
-      ],
+              for (int i = 0; i < widgets.length; i++) {
+                print(widgets[i].controller.text.isEmpty);
+                print(
+                    '$i value is ${widgets[i].firstCheckValue}, ${widgets[i].secondCheckValue}, ${widgets[i].thirdCheckValue}, ${widgets[i].initialValue.round()}, ${widgets[i].controller.text},');
+              }
+            },
+          )
+        ],
+      ),
     );
   }
 }
 
-
-
 // ignore: must_be_immutable
 class DynamicWidget extends StatefulWidget {
-  bool checkValue;
-  DynamicWidget({this.checkValue});
-  String getString() {
-    return 'DynamicWidget(checkValue: $checkValue)';
-  }
+  TextEditingController controller;
+  bool firstCheckValue;
+  bool secondCheckValue;
+  bool thirdCheckValue;
+  double initialValue;
+  DynamicWidget(
+      {this.firstCheckValue,
+      this.secondCheckValue,
+      this.thirdCheckValue,
+      this.initialValue,
+      this.controller});
 
   @override
   _DynamicWidgetState createState() => _DynamicWidgetState();
@@ -99,15 +101,61 @@ class _DynamicWidgetState extends State<DynamicWidget> {
 
   @override
   Widget build(BuildContext context) {
-    return Checkbox(
-      value: widget.checkValue,
-      activeColor: Colors.green,
-      checkColor: Colors.red,
-      onChanged: (bool newValue) {
-        setState(() {
-          widget.checkValue = newValue;
-        });
-      },
+    return Column(
+      children: [
+        TextFormField(
+          controller: widget.controller,
+          decoration: InputDecoration(hintText: 'Enter Name'),
+        ),
+        Row(
+          children: <Widget>[
+            Checkbox(
+              value: widget.firstCheckValue,
+              activeColor: Colors.green,
+              checkColor: Colors.red,
+              onChanged: (bool newValue) {
+                setState(() {
+                  widget.firstCheckValue = newValue;
+                });
+              },
+            ),
+            Checkbox(
+              value: widget.secondCheckValue,
+              activeColor: Colors.green,
+              checkColor: Colors.red,
+              onChanged: (bool newValue) {
+                setState(() {
+                  widget.secondCheckValue = newValue;
+                });
+              },
+            ),
+            Checkbox(
+              value: widget.thirdCheckValue,
+              activeColor: Colors.green,
+              checkColor: Colors.red,
+              onChanged: (bool newValue) {
+                setState(() {
+                  widget.thirdCheckValue = newValue;
+                });
+              },
+            ),
+          ],
+        ),
+        Slider(
+          value: widget.initialValue,
+          divisions: 10,
+          min: 1.0,
+          max: 10.0,
+          activeColor: Colors.lightBlue,
+          inactiveColor: Colors.lightBlue[50],
+          label: '${widget.initialValue.round()}',
+          onChanged: (double newValue) {
+            setState(() {
+              widget.initialValue = newValue;
+            });
+          },
+        ),
+      ],
     );
   }
 }
